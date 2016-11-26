9d08e0e571bc922d1742374a1ca4d80652a72d68
Swolebrain <Vmoreno@columbushs.com>
2016-11-25 19:40:32 -0500

    before models

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 2999;
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  database: 'erm',
-  user: 'root',
-  password: ''
-});
-connection.connect();
-app.use(express.static('static'));
+const PORT = 2998;
 
-app.get('/employers', function(req, res){
-  connection.query('SELECT * FROM employers', function(err, result){
-    if (err) {
-      console.log(err.toString());
-      return res.send(500);
-    }
-    console.log(result);
-    res.json(result);
-  });
-});
+app.use(express.static("static"));
+
 
 
 app.listen(PORT, function(){
-  console.log("server listening on port "+PORT);
+  console.log("Server listening on port "+PORT);
 });
