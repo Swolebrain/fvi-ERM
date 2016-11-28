9b9d91f6f354708d81967ed5b87dcd4dd04f15e3
Swolebrain <Vmoreno@columbushs.com>
2016-11-28 12:04:42 -0500

    syncing up

--- static/js/app.js
+++ static/js/app.js
@@ -6,8 +6,11 @@ angular.module('ERM', [])
   function saveEmployer(emp){
     return $http.put('api/employers/'+emp._id, emp).catch(err=>console.log(err.toString()));
   }
+  function deleteEmployer(id){
+    return $http.delete('api/employers/'+id).catch(err=>console.log(err.toString()));
+  }
 
-  return {getAllEmployers, saveEmployer};
+  return {getAllEmployers, saveEmployer, deleteEmployer};
 }])
 .controller('MainController', ['$scope', 'employersService', function($scope, employersService){
   console.log("main controller is running");
@@ -16,17 +19,25 @@ angular.module('ERM', [])
   $scope.employers = [];
   $scope.currentEmployer = {};
   $scope.saveCurrentEmployer = () => saveEmployer(employersService, $scope.currentEmployer);
+  $scope.deleteCurrentEmployer = () => deleteEmployer(employersService, $scope);
 }]);
 
 
-
+function deleteEmployer(employersService, $scope){
+  var emp = $scope.currentEmployer;
+  if (emp._id){
+    employersService.deleteEmployer(emp._id).then(res=>{
+      console.log(res);
+      $scope.employers = $scope.employers.filter(el=>el != emp);
+    });
+  }
+  $scope.currentEmployer = {};
+}
 
 function employerCardClick($scope, employer){
   $scope.currentEmployer = employer;
 }
 
 function saveEmployer(employersService, employer){
-  console.log("going to save...");
-  console.log(employer);
   employersService.saveEmployer(employer).then(res=>console.log(res));
 }
